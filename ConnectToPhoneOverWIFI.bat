13369e0f030bd19f8165ffeb30c818ff86716631
CasperNoren <casper.noren01@gmail.com>
2022-12-18 16:33:12 +0100

    Changed echo

--- ConnectToPhoneOverWIFI.bat
+++ ConnectToPhoneOverWIFI.bat
@@ -2,12 +2,11 @@ cd %LOCALAPPDATA%\Android\sdk\platform-tools
 @echo off 
 
 SET phoneIp=192.168.0.
-echo %phoneIp%
 
 SET /P phoneIpEnd=Enter last part of ip: 
 
 SET phoneIp=%phoneIp%%phoneIpEnd%:
-echo %phoneIp%
+echo Phone IP set to: %phoneIp%
 
 SET /P pairPort=Enter pair port: 
 echo Pairing on: %phoneIp%%pairPort%
